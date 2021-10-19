0bbe1c4f56a2d4dc6ea9fd79d1e53824931c927f
Fang He <hf.enigma@gmail.com>
2021-10-19 10:57:12 +0800

    p496

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -48,6 +48,7 @@ pub mod p374;
 pub mod p437;
 pub mod p442;
 pub mod p46;
+pub mod p496;
 pub mod p509;
 pub mod p542;
 pub mod p543;
---
+++ rust/src/problems/p496.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let mut pos = HashMap::new();
+        let mut stack = Vec::new();
+        for n in nums2 {
+            while !stack.is_empty() && n > *stack.last().unwrap() {
+                pos.insert(stack.pop().unwrap(), n);
+            }
+            stack.push(n);
+        }
+        nums1
+            .iter()
+            .map(|n1| pos.get(n1).cloned().unwrap_or(-1))
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let nums1 = vec![4, 1, 2];
+        let nums2 = vec![1, 3, 4, 2];
+        let expected = vec![-1, 3, -1];
+        assert_eq!(expected, Solution::next_greater_element(nums1, nums2));
+    }
+
+    #[test]
+    fn case2() {
+        let nums1 = vec![2, 4];
+        let nums2 = vec![1, 2, 3, 4];
+        let expected = vec![3, -1];
+        assert_eq!(expected, Solution::next_greater_element(nums1, nums2));
+    }
+}
