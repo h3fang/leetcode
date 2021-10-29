fa69b5378c7189e5014703948fae1ebae8433789
Fang He <hf.enigma@gmail.com>
2021-10-29 09:04:03 +0800

    remove unnecessary iteration

--- rust/src/problems/p994.rs
+++ rust/src/problems/p994.rs
@@ -6,29 +6,34 @@ impl Solution {
             let old = grid.clone();
             let m = old.len();
             let n = old[0].len();
+            let mut changed = false;
             for i in 0..m {
                 for j in 0..n {
                     if grid[i][j] == 1 {
                         if i > 0 && old[i - 1][j] == 2 {
                             grid[i][j] = 2;
+                            changed = true;
                             continue;
                         }
                         if j > 0 && old[i][j - 1] == 2 {
                             grid[i][j] = 2;
+                            changed = true;
                             continue;
                         }
                         if i < m - 1 && old[i + 1][j] == 2 {
                             grid[i][j] = 2;
+                            changed = true;
                             continue;
                         }
                         if j < n - 1 && old[i][j + 1] == 2 {
                             grid[i][j] = 2;
+                            changed = true;
                         }
                     }
                 }
             }
 
-            grid != &old
+            changed
         }
 
         let mut i = 0;
