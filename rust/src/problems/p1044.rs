f1b758498f3a3c1510481e69081bd32e20aa0dae
Fang He <hf.enigma@gmail.com>
2021-10-30 20:11:17 +0800

    remove unnecessary modulus

--- rust/src/problems/p1044.rs
+++ rust/src/problems/p1044.rs
@@ -18,8 +18,7 @@ impl Solution {
             sub_strs.insert(h, &s[..k]);
 
             for i in 1..=(s.len() - k) {
-                h = ((h * 26 - (s[i - 1] as i64 * m) % MODULUS + MODULUS) % MODULUS
-                    + s[i + k - 1] as i64)
+                h = (h * 26 - (s[i - 1] as i64 * m) % MODULUS + MODULUS + s[i + k - 1] as i64)
                     % MODULUS;
                 if let Some(sub) = sub_strs.get(&h) {
                     if *sub == &s[i..i + k] {
