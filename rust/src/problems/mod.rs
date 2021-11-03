5df33e499e7ba54686620c30c977cd4760e2d349
Fang He <hf.enigma@gmail.com>
2021-11-03 15:28:46 +0800

    p322

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -82,6 +82,7 @@ pub mod p304;
 pub mod p307;
 pub mod p309;
 pub mod p319;
+pub mod p322;
 pub mod p33;
 pub mod p332;
 pub mod p334;
---
+++ rust/src/problems/p322.rs
@@ -0,0 +1,66 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let max = amount + 1;
+        let mut dp = vec![max; amount as usize + 1];
+        dp[0] = 0;
+        let amount = amount as usize;
+        for i in 0..=amount {
+            if dp[i] == max {
+                continue;
+            }
+            for c in &coins {
+                let j = i + *c as usize;
+                if j <= amount {
+                    dp[j] = dp[j].min(dp[i] + 1);
+                }
+            }
+        }
+        if dp[amount] == max {
+            -1
+        } else {
+            dp[amount]
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let coins = vec![1, 2, 5];
+        let amount = 11;
+        assert_eq!(3, Solution::coin_change(coins, amount));
+    }
+
+    #[test]
+    fn case2() {
+        let coins = vec![2];
+        let amount = 3;
+        assert_eq!(-1, Solution::coin_change(coins, amount));
+    }
+
+    #[test]
+    fn case3() {
+        let coins = vec![1];
+        let amount = 0;
+        assert_eq!(0, Solution::coin_change(coins, amount));
+    }
+
+    #[test]
+    fn case4() {
+        let coins = vec![1];
+        let amount = 1;
+        assert_eq!(1, Solution::coin_change(coins, amount));
+    }
+
+    #[test]
+    fn case5() {
+        let coins = vec![1];
+        let amount = 2;
+        assert_eq!(2, Solution::coin_change(coins, amount));
+    }
+}
