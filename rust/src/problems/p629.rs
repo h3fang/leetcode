aa6f0b60b515823bf9f45861b43329492be8b2a8
Fang He <hf.enigma@gmail.com>
2021-11-11 17:07:51 +0800

    p629

---
+++ rust/src/problems/p629.rs
@@ -0,0 +1,41 @@
+pub struct Solution;
+
+const MODULUS: u32 = 1000000007;
+
+impl Solution {
+    pub fn k_inverse_pairs(n: i32, k: i32) -> i32 {
+        let mut dp = vec![vec![0u32; k as usize + 1]; 2];
+        dp[0][0] = 1;
+        let mut c = 1;
+        for i in 1..=n as usize {
+            for j in 0..=k as usize {
+                dp[c][j] = dp[1 - c][j] + if j > 0 { dp[c][j - 1] } else { 0 } + MODULUS
+                    - if j >= i { dp[1 - c][j - i] } else { 0 };
+                dp[c][j] %= MODULUS;
+            }
+            c = 1 - c;
+        }
+
+        dp[1 - c][k as usize] as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(1, Solution::k_inverse_pairs(3, 0));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(2, Solution::k_inverse_pairs(3, 1));
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(663677020, Solution::k_inverse_pairs(1000, 1000));
+    }
+}
