1c81bc15cf5205deb67df2fdadca978540321737
Fang He <hf.enigma@gmail.com>
2021-11-20 10:08:31 +0800

    p540

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -184,6 +184,7 @@ pub mod p500;
 pub mod p509;
 pub mod p520;
 pub mod p54;
+pub mod p540;
 pub mod p542;
 pub mod p543;
 pub mod p547;
---
+++ rust/src/problems/p540.rs
@@ -0,0 +1,44 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn single_non_duplicate(nums: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left < right {
+            let mid = (left + right) / 2;
+            if mid > 0 && nums[mid] == nums[mid - 1] {
+                if mid % 2 == 1 {
+                    left = mid + 1;
+                } else {
+                    right = mid - 2;
+                }
+            } else if mid < nums.len() - 1 && nums[mid] == nums[mid + 1] {
+                if (mid + 1) % 2 == 1 {
+                    left = mid + 2;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                return nums[mid];
+            }
+        }
+        nums[left]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let nums = vec![1, 1, 2, 3, 3, 4, 4, 8, 8];
+        assert_eq!(2, Solution::single_non_duplicate(nums));
+    }
+
+    #[test]
+    fn case2() {
+        let nums = vec![3, 3, 7, 7, 10, 11, 11];
+        assert_eq!(10, Solution::single_non_duplicate(nums));
+    }
+}
