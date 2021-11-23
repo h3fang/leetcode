74f7f48651d101e5e06789d0c2f443ff00e3f41c
Fang He <hf.enigma@gmail.com>
2021-11-23 08:48:22 +0800

    reuse Rng object

--- rust/src/problems/p384.rs
+++ rust/src/problems/p384.rs
@@ -1,25 +1,26 @@
-// use rand::seq::SliceRandom;
-use rand::{thread_rng, Rng};
+use rand::prelude::*;
 
 pub struct Solution {
     nums: Vec<i32>,
+    rng: ThreadRng,
 }
 
 impl Solution {
     pub fn new(nums: Vec<i32>) -> Self {
-        Self { nums }
+        Self {
+            nums,
+            rng: thread_rng(),
+        }
     }
 
     pub fn reset(&self) -> Vec<i32> {
         self.nums.clone()
     }
 
-    pub fn shuffle(&self) -> Vec<i32> {
-        let mut rng = thread_rng();
+    pub fn shuffle(&mut self) -> Vec<i32> {
         let mut r = self.nums.clone();
-        // r.shuffle(&mut rng);
         for i in 0..r.len() {
-            let k = rng.gen_range(i..r.len());
+            let k = self.rng.gen_range(i..r.len());
             r.swap(i, k);
         }
         r
