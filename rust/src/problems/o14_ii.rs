6d67f23361d3939c83864982145147acb8c5a561
Fang He <hf.enigma@gmail.com>
2021-11-25 13:04:40 +0800

    o14_ii

---
+++ rust/src/problems/o14_ii.rs
@@ -0,0 +1,47 @@
+pub struct Solution;
+
+const P: u64 = 1000000007;
+
+impl Solution {
+    pub fn cutting_rope(n: i32) -> i32 {
+        fn reminder(mut a: i32) -> u64 {
+            let mut r = 1;
+            let mut x = 3u64;
+            while a > 0 {
+                if a % 2 == 1 {
+                    r = (r * x) % P as u64;
+                }
+                x = (x * x) % P as u64;
+                a /= 2;
+            }
+            r
+        }
+        match n {
+            2 => 1,
+            3 => 2,
+            _ => {
+                let a = n / 3;
+                ((match n % 3 {
+                    1 => reminder(a - 1) * 4,
+                    2 => reminder(a) * 2,
+                    _ => reminder(a),
+                }) % P) as i32
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(1, Solution::cutting_rope(2));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(620946522, Solution::cutting_rope(1000));
+    }
+}
