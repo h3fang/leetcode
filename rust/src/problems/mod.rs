ff418fa32dc1eebd7a70f3c00d6e82b59e44e337
Fang He <hf.enigma@gmail.com>
2021-12-01 12:16:44 +0800

    p680

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -261,6 +261,7 @@ pub mod p67;
 pub mod p673;
 pub mod p677;
 pub mod p68;
+pub mod p680;
 pub mod p69;
 pub mod p695;
 pub mod p70;
---
+++ rust/src/problems/p680.rs
@@ -0,0 +1,56 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn valid_palindrome(s: String) -> bool {
+        fn is_valid(s: &[u8]) -> bool {
+            if s.is_empty() {
+                return true;
+            }
+            let mut left = 0;
+            let mut right = s.len() - 1;
+            while left < right {
+                if s[left] != s[right] {
+                    return false;
+                }
+                left += 1;
+                right -= 1;
+            }
+            true
+        }
+
+        let s = s.as_bytes();
+        if s.is_empty() {
+            return true;
+        }
+        let mut left = 0;
+        let mut right = s.len() - 1;
+        while left < right {
+            if s[left] != s[right] {
+                return is_valid(&s[left + 1..=right]) || is_valid(&s[left..=right - 1]);
+            }
+            left += 1;
+            right -= 1;
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(true, Solution::valid_palindrome("aba".into()));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(true, Solution::valid_palindrome("abca".into()));
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(false, Solution::valid_palindrome("abc".into()));
+    }
+}
