189df3b021e28b826021fa2e3b83e5523575eb38
Fang He <hf.enigma@gmail.com>
2022-01-22 14:58:55 +0800

    p686

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -345,6 +345,7 @@ pub mod p673;
 pub mod p677;
 pub mod p68;
 pub mod p680;
+pub mod p686;
 pub mod p689;
 pub mod p69;
 pub mod p695;
---
+++ rust/src/problems/p686.rs
@@ -0,0 +1,68 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn repeated_string_match(a: String, b: String) -> i32 {
+        fn kmp(a: &[u8], b: &[u8]) -> i32 {
+            let m = a.len();
+            let n = b.len();
+            if n == 0 {
+                return 0;
+            }
+            let mut p = vec![0; n];
+            let mut j = 0;
+            for i in 1..n {
+                while j > 0 && b[i] != b[j] {
+                    j = p[j - 1];
+                }
+                if b[i] == b[j] {
+                    j += 1;
+                }
+                p[i] = j;
+            }
+            let mut i = 0;
+            j = 0;
+            while i - j < m {
+                while j > 0 && a[i % m] != b[j] {
+                    j = p[(j - 1) as usize];
+                }
+                if a[i % m] == b[j] {
+                    j += 1;
+                }
+                if j == n {
+                    return (i + 1 - n) as i32;
+                }
+                i += 1;
+            }
+            -1
+        }
+        let m = a.len() as i32;
+        let n = b.len() as i32;
+        let i = kmp(a.as_bytes(), b.as_bytes());
+        if i == -1 {
+            return -1;
+        }
+        if m - i >= n {
+            return 1;
+        }
+        (n + i - m - 1) / m + 2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let a = "abcd".to_string();
+        let b = "cdabcdab".to_string();
+        assert_eq!(3, Solution::repeated_string_match(a, b));
+    }
+
+    #[test]
+    fn case2() {
+        let a = "a".to_string();
+        let b = "aa".to_string();
+        assert_eq!(2, Solution::repeated_string_match(a, b));
+    }
+}
