f6d2f7ab2ee741b8756086cb75035aaceaafbc7c
Fang He <hf.enigma@gmail.com>
2022-02-10 00:29:24 +0800

    p1405

--- rust/src/problems/p1405.rs
+++ rust/src/problems/p1405.rs
@@ -35,5 +35,44 @@ mod tests {
     use super::*;
 
     #[test]
-    fn case1() {}
+    fn case1() {
+        let a = 1;
+        let b = 1;
+        let c = 7;
+        let result = Solution::longest_diverse_string(a, b, c);
+        let mut na = 0;
+        let mut nb = 0;
+        let mut nc = 0;
+        for r in result.as_bytes() {
+            match r {
+                b'a' => na += 1,
+                b'b' => nb += 1,
+                b'c' => nc += 1,
+                _ => unreachable!(),
+            }
+        }
+        assert!(na <= a && nb <= b && nc <= c);
+        assert_eq!(result.len(), 8);
+    }
+
+    #[test]
+    fn case2() {
+        let a = 2;
+        let b = 2;
+        let c = 1;
+        let result = Solution::longest_diverse_string(a, b, c);
+        let mut na = 0;
+        let mut nb = 0;
+        let mut nc = 0;
+        for r in result.as_bytes() {
+            match r {
+                b'a' => na += 1,
+                b'b' => nb += 1,
+                b'c' => nc += 1,
+                _ => unreachable!(),
+            }
+        }
+        assert!(na <= a && nb <= b && nc <= c);
+        assert_eq!(result.len(), 5);
+    }
 }
