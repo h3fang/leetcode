c44ab0db123b4411fa6912cd700b82e46fda8957
Fang He <hf.enigma@gmail.com>
2022-03-26 10:53:33 +0800

    apply clippy suggestions

--- rust/src/problems/p704.rs
+++ rust/src/problems/p704.rs
@@ -1,23 +1,17 @@
 pub struct Solution;
 
 impl Solution {
-    #[allow(clippy::comparison_chain)]
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
         let mut a = 0i32;
         let mut b = nums.len() as i32 - 1;
-
         while a <= b {
             let i = (b + a) / 2;
-            let n = nums[i as usize];
-            if n > target {
-                b = i - 1;
-            } else if n < target {
-                a = i + 1;
-            } else {
-                return i as i32;
+            match nums[i as usize].cmp(&target) {
+                std::cmp::Ordering::Less => a = i + 1,
+                std::cmp::Ordering::Equal => return i as i32,
+                std::cmp::Ordering::Greater => b = i - 1,
             }
         }
-
         -1
     }
 }
