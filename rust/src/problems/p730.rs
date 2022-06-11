065bd7517ce76d312e82992342354dba600caeeb
Fang He <hf.enigma@gmail.com>
2022-06-11 10:31:20 +0800

    two dimensional DP

--- rust/src/problems/p730.rs
+++ rust/src/problems/p730.rs
@@ -6,28 +6,47 @@ impl Solution {
     pub fn count_palindromic_subsequences(s: String) -> i32 {
         let s = s.as_bytes();
         let n = s.len();
-        let mut dp = vec![vec![vec![0; n]; n]; 4];
-        for (i, c) in s.iter().enumerate() {
-            let x = (c - b'a') as usize;
-            dp[x][i][i] = 1;
+        let mut dp = vec![vec![0; n]; n];
+        for (i, d) in dp.iter_mut().enumerate() {
+            d[i] = 1;
         }
+        let mut prev = vec![[0; 4]; n];
+        let mut next = vec![[0; 4]; n];
+
+        let mut pos = [-1; 4];
+        for (i, &b) in s.iter().enumerate() {
+            prev[i] = pos;
+            pos[(b - b'a') as usize] = i as i32;
+        }
+
+        pos = [n as i32; 4];
+        for (i, &b) in s.iter().enumerate().rev() {
+            next[i] = pos;
+            pos[(b - b'a') as usize] = i as i32;
+        }
+
         for dist in 2..=n {
             for j in dist - 1..n {
                 let i = j + 1 - dist;
-                for (x, c) in (b'a'..=b'd').enumerate() {
-                    if s[i] == c && s[j] == c {
-                        dp[x][i][j] = (2 + dp.iter().map(|d| d[i + 1][j - 1]).sum::<i64>()) % MOD;
-                    } else if s[i] == c {
-                        dp[x][i][j] = dp[x][i][j - 1];
-                    } else if s[j] == c {
-                        dp[x][i][j] = dp[x][i + 1][j];
-                    } else {
-                        dp[x][i][j] = dp[x][i + 1][j - 1];
-                    }
+                if s[i] == s[j] {
+                    let x = (s[i] - b'a') as usize;
+                    let low = next[i][x];
+                    let high = prev[j][x];
+                    dp[i][j] = match low.cmp(&high) {
+                        std::cmp::Ordering::Less => {
+                            (dp[i + 1][j - 1] * 2 - dp[(low + 1) as usize][(high - 1) as usize]
+                                + MOD)
+                                % MOD
+                        }
+                        std::cmp::Ordering::Equal => (1 + dp[i + 1][j - 1] * 2) % MOD,
+                        std::cmp::Ordering::Greater => (2 + dp[i + 1][j - 1] * 2) % MOD,
+                    };
+                } else {
+                    dp[i][j] = (dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1] + MOD) % MOD;
                 }
             }
         }
-        (dp.iter().map(|d| d[0][n - 1]).sum::<i64>() % MOD) as i32
+        dp[0][n - 1] as i32
     }
 }
 
