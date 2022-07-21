6bf37cf5ce1b7ae729e408b764545ab93eab8a66
Fang He <hf.enigma@gmail.com>
2022-07-21 19:59:54 +0800

    fix p2344

--- rust/src/problems/p2344.rs
+++ rust/src/problems/p2344.rs
@@ -1,7 +1,5 @@
 pub struct Solution;
 
-use std::collections::BTreeMap;
-
 fn gcd(a: i32, b: i32) -> i32 {
     if a == 0 {
         b
@@ -11,32 +9,17 @@ fn gcd(a: i32, b: i32) -> i32 {
 }
 
 impl Solution {
-    pub fn min_operations(nums: Vec<i32>, nums_divide: Vec<i32>) -> i32 {
-        let min = *nums_divide.iter().min().unwrap();
-        let mut min_gcd = min;
-        for &n in &nums_divide {
-            let g = gcd(min, n);
-            min_gcd = min_gcd.min(g);
-        }
+    pub fn min_operations(mut nums: Vec<i32>, nums_divide: Vec<i32>) -> i32 {
+        let g = nums_divide[0];
+        let g = nums_divide.into_iter().fold(g, |g, n| gcd(g, n));
 
-        let mut m = BTreeMap::new();
-        for &n in &nums {
-            *m.entry(n).or_insert(0) += 1;
-        }
-        let mut valid = false;
-        let mut count = 0;
-        for (n, c) in m {
-            if min_gcd % n == 0 {
-                valid = true;
-                break;
+        nums.sort_unstable();
+        for (i, &n) in nums.iter().enumerate() {
+            if g % n == 0 {
+                return i as i32;
             }
-            count += c;
         }
-        if valid {
-            count
-        } else {
-            -1
-        }
+        -1
     }
 }
 
