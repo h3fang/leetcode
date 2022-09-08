81d02bfc8951445903d24b3c34ed04706816b6f1
Fang He <hf.enigma@gmail.com>
2022-09-08 08:36:37 +0800

    p2396

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -498,6 +498,7 @@ pub mod p2390;
 pub mod p2391;
 pub mod p2392;
 pub mod p2395;
+pub mod p2396;
 pub mod p24;
 pub mod p240;
 pub mod p241;
---
+++ rust/src/problems/p2396.rs
@@ -0,0 +1,47 @@
+pub struct Solution;
+
+fn format_radix(mut x: u32, radix: u32) -> String {
+    let mut result = vec![];
+
+    loop {
+        let m = x % radix;
+        x /= radix;
+
+        result.push(std::char::from_digit(m, radix).unwrap());
+        if x == 0 {
+            break;
+        }
+    }
+    result.into_iter().rev().collect()
+}
+
+impl Solution {
+    pub fn is_strictly_palindromic(n: i32) -> bool {
+        for b in 2..=n - 2 {
+            let s = format_radix(n as u32, b as u32);
+            let s = s.as_bytes();
+            let len = s.len();
+            for i in 0..len / 2 {
+                if s[i] != s[len - 1 - i] {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(false, Solution::is_strictly_palindromic(9));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(false, Solution::is_strictly_palindromic(4));
+    }
+}
