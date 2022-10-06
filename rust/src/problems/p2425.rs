bcd3ac52fa11dd95b54893dee9d98ff6e4c9900b
Fang He <hf.enigma@gmail.com>
2022-10-06 19:20:15 +0800

    p2425

---
+++ rust/src/problems/p2425.rs
@@ -0,0 +1,35 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        let n1 = nums1.len();
+        let n2 = nums2.len();
+        if n1 % 2 == 0 && n2 % 2 == 0 {
+            0
+        } else {
+            let mut result = 0;
+            if n2 % 2 == 1 {
+                result = nums1.iter().fold(0, |acc, n| acc ^ *n);
+            }
+            if n1 % 2 == 1 {
+                result = nums2.iter().fold(result, |acc, n| acc ^ *n);
+            }
+            result
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(13, Solution::xor_all_nums(vec![2, 1, 3], vec![10, 2, 5, 0]));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(0, Solution::xor_all_nums(vec![1, 2], vec![3, 4]));
+    }
+}
