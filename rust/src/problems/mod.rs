f132a3bec04ed35d26bff7f727a6206b50b734b4
Fang He <hf.enigma@gmail.com>
2022-10-17 18:58:21 +0800

    p1832

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -255,6 +255,7 @@ pub mod p1791;
 pub mod p1800;
 pub mod p1816;
 pub mod p1823;
+pub mod p1832;
 pub mod p187;
 pub mod p188;
 pub mod p189;
---
+++ rust/src/problems/p1832.rs
@@ -0,0 +1,29 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn check_if_pangram(sentence: String) -> bool {
+        let mut f = [0; 26];
+        for b in sentence.as_bytes() {
+            f[(b - b'a') as usize] += 1;
+        }
+        f.iter().all(|&e| e >= 1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(
+            true,
+            Solution::check_if_pangram("thequickbrownfoxjumpsoverthelazydog".to_string())
+        );
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(false, Solution::check_if_pangram("leetcode".to_string()));
+    }
+}
