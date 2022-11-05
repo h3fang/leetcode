56590a306a65523786328d5e38c429cfba3ad7ae
Fang He <hf.enigma@gmail.com>
2022-11-05 20:12:56 +0800

    p212 refactor

--- rust/src/problems/p212.rs
+++ rust/src/problems/p212.rs
@@ -1,18 +1,14 @@
+pub struct Solution;
+
+#[derive(Default)]
 struct Trie {
-    next: Vec<Option<Box<Trie>>>,
+    next: [Option<Box<Trie>>; 26],
     word: Option<String>,
 }
 
 impl Trie {
-    fn new() -> Self {
-        Self {
-            next: (0..256).map(|_| None).collect(),
-            word: None,
-        }
-    }
-
     fn from_words(words: Vec<String>) -> Self {
-        let mut t = Self::new();
+        let mut t = Self::default();
         for w in words {
             t.insert(w);
         }
@@ -20,58 +16,42 @@ impl Trie {
     }
 
     fn insert(&mut self, word: String) {
-        let bytes = word.as_bytes();
-        self.insert_impl(bytes, word.to_string());
-    }
-
-    fn insert_impl(&mut self, chars: &[u8], word: String) {
-        if chars.is_empty() {
-            self.word = Some(word);
-            return;
+        let mut t = self;
+        for b in word.as_bytes() {
+            t = t.next[(b - b'a') as usize].get_or_insert_with(Default::default);
         }
-
-        let c = chars[0];
-        match self.next.get_mut(c as usize).unwrap() {
-            Some(t) => {
-                t.insert_impl(&chars[1..], word);
-            }
-            None => {
-                let mut t = Self::new();
-                t.insert_impl(&chars[1..], word);
-                self.next[c as usize] = Some(Box::new(t));
-            }
-        }
+        t.word = Some(word);
     }
 }
 
-pub struct Solution;
-
 impl Solution {
-    pub fn find_words(board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
+    pub fn find_words(mut board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
         fn bt(
             board: &mut Vec<Vec<char>>,
             i: i32,
             j: i32,
-            height: i32,
-            width: i32,
             trie: &mut Trie,
             result: &mut Vec<String>,
         ) {
-            if i < 0 || j < 0 || i >= height || j >= width {
+            if i < 0 || j < 0 || i >= board.len() as i32 || j >= board[0].len() as i32 {
                 return;
             }
 
             let c = board[i as usize][j as usize];
+            if c == '{' {
+                return;
+            }
 
-            if let Some(t) = trie.next.get_mut(c as usize).unwrap() {
+            let idx = (c as u8 - b'a') as usize;
+            if let Some(t) = &mut trie.next[idx] {
                 if let Some(word) = t.word.take() {
                     result.push(word);
                 }
                 board[i as usize][j as usize] = '{';
-                bt(board, i - 1, j, height, width, t, result);
-                bt(board, i + 1, j, height, width, t, result);
-                bt(board, i, j - 1, height, width, t, result);
-                bt(board, i, j + 1, height, width, t, result);
+                bt(board, i - 1, j, t, result);
+                bt(board, i + 1, j, t, result);
+                bt(board, i, j - 1, t, result);
+                bt(board, i, j + 1, t, result);
                 board[i as usize][j as usize] = c;
             }
         }
@@ -80,20 +60,11 @@ impl Solution {
 
         let m = board.len();
         let n = board[0].len();
-        let mut board = board;
 
         let mut result = Vec::new();
         for i in 0..m {
             for j in 0..n {
-                bt(
-                    &mut board,
-                    i as i32,
-                    j as i32,
-                    m as i32,
-                    n as i32,
-                    &mut trie,
-                    &mut result,
-                );
+                bt(&mut board, i as i32, j as i32, &mut trie, &mut result);
             }
         }
         result
