663208d55914ca5e52f3ccb76b32d169b6c9bded
Fang He <hf.enigma@gmail.com>
2022-11-07 16:03:38 +0800

    p816

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -903,6 +903,7 @@ pub mod p81;
 pub mod p811;
 pub mod p812;
 pub mod p814;
+pub mod p816;
 pub mod p817;
 pub mod p819;
 pub mod p82;
---
+++ rust/src/problems/p816.rs
@@ -0,0 +1,83 @@
+pub struct Solution;
+
+use std::borrow::Cow;
+
+impl Solution {
+    pub fn ambiguous_coordinates(s: String) -> Vec<String> {
+        let bytes = s.as_bytes();
+        let n = bytes.len();
+        let mut result = vec![];
+        for c in 2..n - 1 {
+            for a in 2..=c {
+                if (a > 2 && bytes[1] == b'0') || (a < c && bytes[c - 1] == b'0') {
+                    continue;
+                }
+                let x = if a < c {
+                    Cow::Owned(format!("{}.{}", &s[1..a], &s[a..c]))
+                } else {
+                    Cow::Borrowed(&s[1..c])
+                };
+                for b in c + 1..=n - 1 {
+                    if (b > c + 1 && bytes[c] == b'0') || (b < n - 1 && bytes[n - 2] == b'0') {
+                        continue;
+                    }
+                    let y = if b < n - 1 {
+                        Cow::Owned(format!("{}.{}", &s[c..b], &s[b..n - 1]))
+                    } else {
+                        Cow::Borrowed(&s[c..n - 1])
+                    };
+                    result.push(format!("({}, {})", x, y));
+                }
+            }
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let mut result = Solution::ambiguous_coordinates("(123)".to_string());
+        result.sort_unstable();
+        let mut expected = ["(1, 23)", "(12, 3)", "(1.2, 3)", "(1, 2.3)"]
+            .iter()
+            .map(|c| c.to_string())
+            .collect::<Vec<_>>();
+        expected.sort_unstable();
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn case2() {
+        let mut result = Solution::ambiguous_coordinates("(00011)".to_string());
+        result.sort_unstable();
+        let mut expected = ["(0.001, 1)", "(0, 0.011)"]
+            .iter()
+            .map(|c| c.to_string())
+            .collect::<Vec<_>>();
+        expected.sort_unstable();
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn case3() {
+        let mut result = Solution::ambiguous_coordinates("(0123)".to_string());
+        result.sort_unstable();
+        let mut expected = [
+            "(0, 123)",
+            "(0, 12.3)",
+            "(0, 1.23)",
+            "(0.1, 23)",
+            "(0.1, 2.3)",
+            "(0.12, 3)",
+        ]
+        .iter()
+        .map(|c| c.to_string())
+        .collect::<Vec<_>>();
+        expected.sort_unstable();
+        assert_eq!(expected, result);
+    }
+}
