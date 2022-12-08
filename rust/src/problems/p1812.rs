75f5218354d5714273471dac926a1f24d1ac3615
Fang He <hf.enigma@gmail.com>
2022-12-08 16:19:33 +0800

    p1812

---
+++ rust/src/problems/p1812.rs
@@ -0,0 +1,29 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn square_is_white(coordinates: String) -> bool {
+        let i = coordinates.as_bytes()[0] - b'a';
+        let j = coordinates.as_bytes()[1] - b'1';
+        (i + j) % 2 == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(false, Solution::square_is_white("a1".to_string()));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(true, Solution::square_is_white("h3".to_string()));
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(false, Solution::square_is_white("c7".to_string()));
+    }
+}
