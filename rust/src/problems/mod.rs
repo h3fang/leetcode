f5e0473911cb4d0691307cd4389e2c1445a15acc
Fang He <hf.enigma@gmail.com>
2022-12-13 18:35:04 +0800

    p931

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -1018,6 +1018,7 @@ pub mod p926;
 pub mod p927;
 pub mod p929;
 pub mod p93;
+pub mod p931;
 pub mod p933;
 pub mod p934;
 pub mod p936;
---
+++ rust/src/problems/p931.rs
@@ -0,0 +1,44 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn min_falling_path_sum(matrix: Vec<Vec<i32>>) -> i32 {
+        let m = matrix.len();
+        let n = matrix[0].len();
+
+        let mut dp = vec![vec![0; n]; m];
+        dp[m - 1] = matrix[m - 1].clone();
+        for i in (0..m - 1).rev() {
+            for j in 0..n {
+                dp[i][j] = dp[i + 1][j];
+                if j > 0 {
+                    dp[i][j] = dp[i][j].min(dp[i + 1][j - 1]);
+                }
+                if j + 1 < n {
+                    dp[i][j] = dp[i][j].min(dp[i + 1][j + 1]);
+                }
+                dp[i][j] += matrix[i][j];
+            }
+        }
+        *dp[0].iter().min().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let matrix = [[2, 1, 3], [6, 5, 4], [7, 8, 9]]
+            .iter()
+            .map(|r| r.to_vec())
+            .collect();
+        assert_eq!(13, Solution::min_falling_path_sum(matrix));
+    }
+
+    #[test]
+    fn case2() {
+        let matrix = [[-19, 57], [-40, -5]].iter().map(|r| r.to_vec()).collect();
+        assert_eq!(-59, Solution::min_falling_path_sum(matrix));
+    }
+}
