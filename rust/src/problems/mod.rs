d460313321bf354b2ddcfb8d3e27e4ab51ddb2b1
Fang He <hf.enigma@gmail.com>
2023-05-29 12:35:41 +0800

    p2455

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -754,6 +754,7 @@ pub mod p2441;
 pub mod p2444;
 pub mod p2446;
 pub mod p2451;
+pub mod p2455;
 pub mod p2466;
 pub mod p2469;
 pub mod p2477;
---
+++ rust/src/problems/p2455.rs
@@ -0,0 +1,33 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn average_value(nums: Vec<i32>) -> i32 {
+        let (n, sum) = nums.into_iter().fold((0, 0), |(n, sum), i| {
+            if i % 6 == 0 {
+                (n + 1, sum + i)
+            } else {
+                (n, sum)
+            }
+        });
+        if n > 0 {
+            sum / n
+        } else {
+            0
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(9, Solution::average_value(vec![1, 3, 6, 10, 12, 15]));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(0, Solution::average_value(vec![1, 2, 4, 7, 10]));
+    }
+}
