a7d0f2354dce6ab896881bba94bd5f37211f2734
Fang He <hf.enigma@gmail.com>
2024-08-22 23:33:30 +0800

    p3133

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -920,6 +920,7 @@ pub mod p3129;
 pub mod p3130;
 pub mod p3131;
 pub mod p3132;
+pub mod p3133;
 pub mod p3137;
 pub mod p3143;
 pub mod p315;
---
+++ rust/src/problems/p3133.rs
@@ -0,0 +1,34 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn min_end(n: i32, x: i32) -> i64 {
+        let bits = 64 - n.leading_zeros() - x.leading_zeros();
+        let mut result = x as i64;
+        let n = n as i64 - 1;
+        let mut j = 0;
+        for i in 0..bits {
+            if (result >> i & 1) == 0 {
+                if ((n >> j) & 1) != 0 {
+                    result |= 1 << i;
+                }
+                j += 1;
+            }
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(6, Solution::min_end(3, 4));
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(15, Solution::min_end(2, 7));
+    }
+}
