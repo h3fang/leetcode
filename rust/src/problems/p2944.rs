a29bf40745e2d391dfdf0a129200017c2d7708ea
Fang He <hf.enigma@gmail.com>
2025-01-25 01:01:01 +0800

    p2944 optimization

--- rust/src/problems/p2944.rs
+++ rust/src/problems/p2944.rs
@@ -1,19 +1,23 @@
 pub struct Solution;
 
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn minimum_coins(prices: Vec<i32>) -> i32 {
         let n = prices.len();
-        let mut f = vec![i32::MAX; n];
-        f[n - 1] = prices[n - 1];
-        for i in (0..n - 1).rev() {
-            f[i] = prices[i]
-                + if i + i + 2 >= n {
-                    0
-                } else {
-                    *f[i + 1..i + i + 3].iter().min().unwrap()
-                };
+        let mut q = VecDeque::with_capacity(n);
+        q.push_back((n + 1, 0));
+        for i in (1..=n).rev() {
+            while q.back().unwrap().0 > i + i + 1 {
+                q.pop_back();
+            }
+            let p = prices[i - 1] + q.back().unwrap().1;
+            while p < q.front().unwrap().1 {
+                q.pop_front();
+            }
+            q.push_front((i, p));
         }
-        f[0]
+        q.front().unwrap().1
     }
 }
 
