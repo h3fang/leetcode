3b22b290ca44b158361a745c9006247963e4cff0
Fang He <hf.enigma@gmail.com>
2025-03-03 10:51:36 +0800

    p300: use Vec::partition_point

--- rust/src/problems/p300.rs
+++ rust/src/problems/p300.rs
@@ -2,33 +2,20 @@ pub struct Solution;
 
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        let mut d = vec![0; nums.len() + 1];
-        d[1] = nums[0];
-        let mut len = 1;
+        let mut d = Vec::with_capacity(nums.len() + 1);
+        d.push(i32::MIN);
+        d.push(nums[0]);
 
         for &e in nums.iter().skip(1) {
-            if e > d[len] {
-                len += 1;
-                d[len] = e;
+            if e > *d.last().unwrap() {
+                d.push(e);
             } else {
-                let mut a = 1;
-                let mut b = len;
-                let mut p = 0;
-                while a <= b {
-                    let mid = a + (b - a) / 2;
-                    match d[mid].cmp(&e) {
-                        std::cmp::Ordering::Less => {
-                            p = mid;
-                            a = mid + 1;
-                        }
-                        _ => b = mid - 1,
-                    }
-                }
-                d[p + 1] = e;
+                let p = d.partition_point(|&x| x < e);
+                d[p] = e;
             }
         }
 
-        len as i32
+        d.len() as i32 - 1
     }
 }
 
