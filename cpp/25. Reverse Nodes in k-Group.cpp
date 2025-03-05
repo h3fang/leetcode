935b76df26d9bfabdf7fb4ea209e67e5bd10ab8c
Fang He <hf.enigma@gmail.com>
2025-03-05 15:40:23 +0800

    p25: c++ rewrite

--- cpp/25. Reverse Nodes in k-Group.cpp
+++ cpp/25. Reverse Nodes in k-Group.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
+#include <cassert>
 #include <vector>
-#include <cstdio>
 
 #include "helpers.h"
 
@@ -8,7 +8,7 @@ using namespace std;
 
 class Solution {
 public:
-    void reverse(ListNode* h, ListNode* t) {
+    void reverse(ListNode *h, ListNode *t) {
         ListNode *p = nullptr;
         while (h != t) {
             auto a = h->next;
@@ -18,43 +18,38 @@ public:
         }
         t->next = p;
     }
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        if (k == 1) {return head;}
-        ListNode d, *r = &d;
-        ListNode* t,*h = head;
-        const int m = k;
-        while (head) {
-            if (m == k) {
-                h = head;
-            }
-            k--;
-            if (k == 0) {
-                t = head;
-                head = head->next;
-                reverse(h, t);
-                r->next = t;
-                r = h;
-                k = m;
-                h = head;
-            }
-            else {
-                head = head->next;
-            }
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        if (k == 1) {
+            return head;
         }
-        if (k < m) r->next = h;
-        else r->next = nullptr;
+        ListNode d(0, head), *prev = &d;
+        while (head) {
+            auto tail = head;
+            for (int i = 1; i < k; i++) {
+                tail = tail->next;
+                if (!tail) {
+                    return d.next;
+                }
+            }
+            auto next = tail->next;
+            reverse(head, tail);
+            prev->next = tail;
+            head->next = next;
+            prev = head;
+            head = next;
+        }
         return d.next;
     }
 };
 
 int main() {
-    vector<int> inputs = {1,2,3,4,5};
-
-    auto h = Solution().reverseKGroup(parse_singly_linked_list(inputs), 3);
-    while (h) {
-        printf("%d ", h->val);
-        h = h->next;
+    auto r = Solution().reverseKGroup(parse_singly_linked_list({1, 2, 3, 4, 5}), 2);
+    ListNode *expected = parse_singly_linked_list({2, 1, 4, 3, 5});
+    while (expected) {
+        assert(r && r->val == expected->val);
+        expected = expected->next;
+        r = r->next;
     }
-    printf("\n");
+    assert(!r);
     return 0;
 }
