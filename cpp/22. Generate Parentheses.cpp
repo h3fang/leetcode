0e6efde2d5abb7d87e977b1aba398071d842deed
Fang He <hf.enigma@gmail.com>
2025-03-07 00:21:05 +0800

    p22: c++ rewrite

--- cpp/22. Generate Parentheses.cpp
+++ cpp/22. Generate Parentheses.cpp
@@ -1,53 +1,42 @@
 #include <algorithm>
-#include <vector>
+#include <cassert>
 #include <string>
-#include <map>
-#include <set>
-#include <cstdio>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-    vector<string> res;
-    void gp(int n, int m, int a, int b, int l, int r, string& s) {
-        if (n==0 && m==0) {
-            res.push_back(s);
+    vector<string> ans;
+    void bt(int n, int i, int left, string &s) {
+        if (i == n) {
+            ans.push_back(s);
             return;
         }
-
-        if (n>0 && m>0) {
-            s[a] = '('; s[b] = ')';
-            gp(n-1, m-1, a+1, b-1, l+1, r+1, s);
-
-
-            if( l > 0 && r > 0) {
-                s[a] = ')'; s[b] = '(';
-                gp(n-1, m-1, a+1, b-1, l-1, r-1, s);
-            }
+        if (left * 2 < n) {
+            s.push_back('(');
+            bt(n, i + 1, left + 1, s);
+            s.pop_back();
         }
-
-        if( n > 1 && r > 0) {
-            s[a] = '('; s[b] = '(';
-            gp(n-2, m, a+1, b-1, l+1, r-1, s);
-        }
-
-        if( m > 1 && l > 0) {
-            s[a] = ')'; s[b] = ')';
-            gp(n, m-2, a+1, b-1, l-1, r+1, s);
+        if (i - left < left) {
+            s.push_back(')');
+            bt(n, i + 1, left, s);
+            s.pop_back();
         }
     }
+
+public:
     vector<string> generateParenthesis(int n) {
-        if (n==0) return vector<string>{""};
-        string s(2*n, 0);
-        gp(n, n, 0, 2*n-1, 0, 0, s);
-        return res;
+        string s;
+        bt(2 * n, 0, 0, s);
+        return ans;
     }
 };
 
 int main() {
-    for (auto& e : Solution().generateParenthesis(10)) {
-        printf("%s\n", e.data());
-    }
+    auto r = Solution().generateParenthesis(3);
+    sort(r.begin(), r.end());
+    vector<string> expected = {"((()))", "(()())", "(())()", "()(())", "()()()"};
+    sort(expected.begin(), expected.end());
+    assert(r == expected);
     return 0;
 }
