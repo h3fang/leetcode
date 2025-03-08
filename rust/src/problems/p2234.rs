946d67da5f73d9d30ccdc2f38b269d31838f3c57
Fang He <hf.enigma@gmail.com>
2025-03-08 14:25:17 +0800

    p2234: refactoring

--- rust/src/problems/p2234.rs
+++ rust/src/problems/p2234.rs
@@ -8,58 +8,38 @@ impl Solution {
         full: i32,
         partial: i32,
     ) -> i64 {
-        fn bisec_right(arr: &[i64], target: i64) -> usize {
-            let mut left = 0;
-            let mut right = arr.len() - 1;
-            let mut result = arr.len();
-            while left <= right {
-                let mid = (left + right) / 2;
-                match arr[mid].cmp(&target) {
-                    std::cmp::Ordering::Greater => {
-                        result = mid;
-                        right = mid - 1;
-                    }
-                    _ => left = mid + 1,
-                }
-            }
-            result
-        }
-
-        flowers.iter_mut().for_each(|f| *f = (*f).min(target));
         flowers.sort_unstable();
 
         let n = flowers.len() as i64;
         let full = full as i64;
         let partial = partial as i64;
 
-        let mut j = flowers.len() - 1;
-        while flowers[j] == target {
-            if j == 0 {
-                return full * n;
-            }
-            j -= 1;
+        let mut f = flowers.partition_point(|&x| x < target);
+        if f == 0 {
+            return full * n;
         }
 
-        let sum = flowers.iter().fold(0i64, |acc, &n| acc + n as i64);
-        if new_flowers >= target as i64 * n - sum {
-            return (full * n).max(full * (n - 1) + partial * (target as i64 - 1));
+        let mut cost = vec![0; f];
+        for i in 1..f {
+            cost[i] = cost[i - 1] + (i as i64) * (flowers[i] - flowers[i - 1]) as i64;
+        }
+        if cost[f - 1] + (target - flowers[f - 1]) as i64 * f as i64 <= new_flowers {
+            return (full * n).max(full * (n - 1) + (target as i64 - 1) * partial);
         }
 
-        let mut cost = vec![0; flowers.len()];
-        for i in 1..flowers.len() {
-            cost[i] = cost[i - 1] + (flowers[i] - flowers[i - 1]) as i64 * i as i64;
-        }
+        f -= 1;
+
         let mut result = 0;
         while new_flowers > 0 {
-            let idx = j.min(bisec_right(&cost, new_flowers) - 1);
-            let top = (flowers[idx] as i64 + (new_flowers - cost[idx]) / (idx as i64 + 1))
+            let p = cost[..=f].partition_point(|&x| x <= new_flowers) - 1;
+            let top = (flowers[p] as i64 + (new_flowers - cost[p]) / (p as i64 + 1))
                 .min(target as i64 - 1);
-            result = result.max(full * (flowers.len() - j - 1) as i64 + partial * top);
-            new_flowers -= (target - flowers[j]) as i64;
-            if j == 0 {
+            result = result.max(full * (n - f as i64 - 1) + partial * top);
+            new_flowers -= (target - flowers[f]) as i64;
+            if f == 0 {
                 break;
             }
-            j -= 1;
+            f -= 1;
         }
         result
     }
@@ -77,5 +57,13 @@ mod tests {
     #[test]
     fn case2() {
         assert_eq!(30, Solution::maximum_beauty(vec![2, 4, 5, 3], 10, 5, 2, 6));
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(
+            58,
+            Solution::maximum_beauty(vec![5, 5, 15, 1, 9], 36, 12, 9, 2)
+        );
     }
 }
