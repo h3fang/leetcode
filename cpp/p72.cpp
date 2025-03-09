61f867494400471f1009b145889879c22de6b3dc
Fang He <hf.enigma@gmail.com>
2025-03-09 21:19:55 +0800

    p72: c++ optimize memory usage

--- cpp/p72.cpp
+++ cpp/p72.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <cassert>
 #include <string>
 #include <vector>
 
@@ -8,28 +8,29 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         const int N1 = word1.size(), N2 = word2.size();
-        vector<vector<int>> dp(N1 + 1, vector<int>(N2 + 1));
-        for (int i = 0; i < N1 + 1; i++) {
-            dp[i][0] = i;
-        }
+        vector<int> dp(N2 + 1, 0);
         for (int i = 0; i < N2 + 1; i++) {
-            dp[0][i] = i;
+            dp[i] = i;
         }
         for (int i = 1; i < N1 + 1; i++) {
+            dp[0] = i - 1;
+            int pre = i - 1;
             for (int j = 1; j < N2 + 1; j++) {
+                int temp = dp[j];
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[j] = pre;
                 } else {
-                    dp[i][j] = min(min(dp[i - 1][j - 1] + 1, dp[i][j - 1] + 1), dp[i - 1][j] + 1);
+                    dp[j] = min(min(pre + 1, dp[j - 1] + 1), dp[j] + 1);
                 }
+                pre = temp;
             }
         }
-        return dp[N1][N2];
+        return dp[N2];
     }
 };
 
 int main() {
-    string a = "horse", b = "ros";
-    printf("%d\n", Solution().minDistance(a, b));
+    assert(3 == Solution().minDistance("horse", "ros"));
+    assert(5 == Solution().minDistance("intention", "execution"));
     return 0;
 }
