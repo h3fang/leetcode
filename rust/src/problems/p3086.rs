93142fe508a1e5ee0d3f2895feca8c18e6672a83
Fang He <hf.enigma@gmail.com>
2025-04-14 12:38:05 +0800

    p3086: fix s2 and final result calculation, minor refactor

--- rust/src/problems/p3086.rs
+++ rust/src/problems/p3086.rs
@@ -8,14 +8,12 @@ impl Solution {
             if x == 0 {
                 c = c.max(curr);
                 curr = 0;
-                continue;
             } else {
                 curr += 1;
                 ones.push(i);
             }
         }
-        c = c.max(curr);
-        c = c.min(3).min(k);
+        c = c.max(curr).min(3).min(k);
         if max_changes >= k - c {
             return (0.max(c - 1) + 2 * (k - c)) as i64;
         }
@@ -28,12 +26,12 @@ impl Solution {
         for r in size..=ones.len() {
             let l = r - size;
             let i = l + size / 2;
-            let j = ones[i];
-            let s1 = j * (i - l) - (presum[i] - presum[l]);
-            let s2 = presum[r] - presum[i] - i * (r - i);
+            let v = ones[i];
+            let s1 = v * (i - l) - (presum[i] - presum[l]);
+            let s2 = presum[r] - presum[i] - v * (r - i);
             result = result.min((s1 + s2) as i64);
         }
-        result
+        result + max_changes as i64 * 2
     }
 }
 
@@ -52,5 +50,10 @@ mod tests {
     #[test]
     fn case2() {
         assert_eq!(4, Solution::minimum_moves(vec![0, 0, 0, 0], 2, 3));
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(4, Solution::minimum_moves(vec![1, 0, 1, 0, 1], 3, 0));
     }
 }
