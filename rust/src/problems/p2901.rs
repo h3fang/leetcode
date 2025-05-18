e696601caca07250ac3d460df73c66bb7c6ea73a
Fang He <hf.enigma@gmail.com>
2025-05-18 10:02:35 +0800

    p2901: optimization

--- rust/src/problems/p2901.rs
+++ rust/src/problems/p2901.rs
@@ -1,36 +1,89 @@
 pub struct Solution;
 
-fn distance(a: &str, b: &str) -> usize {
-    a.bytes().zip(b.bytes()).filter(|(a, b)| a != b).count()
+use std::collections::HashMap;
+
+fn hash(w: &[u8]) -> u64 {
+    let mut ans = 0;
+    for (i, b) in w.iter().enumerate() {
+        ans |= ((b & 31) as u64) << (i * 5);
+    }
+    ans
 }
 
-fn check(words: &[String], groups: &[i32], i: usize, j: usize) -> bool {
-    groups[i] != groups[j]
-        && words[i].len() == words[j].len()
-        && distance(&words[i], &words[j]) == 1
+fn max_f(
+    i: usize,
+    w: &str,
+    g: i32,
+    h: u64,
+    groups: &[i32],
+    from: &mut [usize],
+    m: &mut HashMap<u64, (usize, usize, usize, usize)>,
+) -> usize {
+    let mut f = 0;
+    for k in 0..w.len() {
+        let h1 = h | (31 << (k * 5));
+        let (max1, j1, max2, j2) = m.get(&h1).cloned().unwrap_or((0, 0, 0, 0));
+        if g != groups[j1] {
+            if max1 > f {
+                f = max1;
+                from[i] = j1;
+            }
+        } else if max2 > f {
+            f = max2;
+            from[i] = j2;
+        }
+    }
+    f + 1
+}
+
+fn update_f(
+    i: usize,
+    w: &str,
+    g: i32,
+    h: u64,
+    f: usize,
+    groups: &[i32],
+    m: &mut HashMap<u64, (usize, usize, usize, usize)>,
+) {
+    for k in 0..w.len() {
+        let h1 = h | (31 << (k * 5));
+        let (mut max1, mut j1, mut max2, mut j2) = m.get(&h1).cloned().unwrap_or((0, 0, 0, 0));
+        if f > max1 {
+            if g != groups[j1] {
+                max2 = max1;
+                j2 = j1;
+            }
+            max1 = f;
+            j1 = i;
+        } else if f > max2 && g != groups[j1] {
+            max2 = f;
+            j2 = i;
+        }
+        m.insert(h1, (max1, j1, max2, j2));
+    }
 }
 
 impl Solution {
     pub fn get_words_in_longest_subsequence(words: Vec<String>, groups: Vec<i32>) -> Vec<String> {
         let n = words.len();
-        let mut f = vec![(1, 0); n];
-        for i in 1..n {
-            let mut max = (1, 0);
-            for (j, e) in f[..i].iter().enumerate() {
-                if check(&words, &groups, i, j) && max.0 < e.0 + 1 {
-                    max = (e.0 + 1, j);
-                }
+        let mut m = HashMap::with_capacity(2048);
+        let mut from = vec![0; n];
+        let (mut max, mut max_i) = (0, 0);
+        for (i, (w, &g)) in words.iter().zip(&groups).enumerate().rev() {
+            let h = hash(w.as_bytes());
+            let f = max_f(i, w, g, h, &groups, &mut from, &mut m);
+            if f > max {
+                max = f;
+                max_i = i;
             }
-            f[i] = max;
+            update_f(i, w, g, h, f, &groups, &mut m);
         }
-        let (mut i, (mut max, _)) = f.iter().cloned().enumerate().max_by_key(|e| e.1).unwrap();
+
         let mut ans = Vec::with_capacity(max);
-        while max > 0 {
-            ans.push(words[i].to_string());
-            i = f[i].1;
-            max -= 1;
+        for _ in 0..max {
+            ans.push(words[max_i].to_string());
+            max_i = from[max_i];
         }
-        ans.reverse();
         ans
     }
 }
