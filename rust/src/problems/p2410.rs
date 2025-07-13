412657aca978b6181019d1d0e410af7acb166f54
Fang He <hf.enigma@gmail.com>
2025-07-13 17:26:58 +0800

    p2410: optimization

--- rust/src/problems/p2410.rs
+++ rust/src/problems/p2410.rs
@@ -1,25 +1,17 @@
 pub struct Solution;
 
-use std::collections::BTreeMap;
-
 impl Solution {
-    pub fn match_players_and_trainers(mut players: Vec<i32>, trainers: Vec<i32>) -> i32 {
+    pub fn match_players_and_trainers(mut players: Vec<i32>, mut trainers: Vec<i32>) -> i32 {
         players.sort_unstable();
-        let mut m = BTreeMap::new();
+        trainers.sort_unstable();
+        let (mut ans, mut i) = (0, 0);
         for t in trainers {
-            *m.entry(t).or_insert(0) += 1;
-        }
-        for (i, &p) in players.iter().enumerate() {
-            if let Some((&t, c)) = m.range_mut(p..).next() {
-                *c -= 1;
-                if *c == 0 {
-                    m.remove(&t);
-                }
-            } else {
-                return i as i32;
+            if i < players.len() && players[i] <= t {
+                ans += 1;
+                i += 1;
             }
         }
-        players.len() as i32
+        ans
     }
 }
 
