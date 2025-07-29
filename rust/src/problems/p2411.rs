0b3441c63d46b0ba5e0e2b2a2a61569c217f2ce4
Fang He <hf.enigma@gmail.com>
2025-07-29 23:30:18 +0800

    p2411: optimization

--- rust/src/problems/p2411.rs
+++ rust/src/problems/p2411.rs
@@ -1,38 +1,26 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn smallest_subarrays(nums: Vec<i32>) -> Vec<i32> {
+    pub fn smallest_subarrays(mut nums: Vec<i32>) -> Vec<i32> {
         let n = nums.len();
-        let mut result = vec![0; n];
-        result[n - 1] = 1;
-        let mut count = [0; 32];
-        for (j, c) in count.iter_mut().enumerate() {
-            if nums[n - 1] & (1 << j) > 0 {
-                *c += 1;
-            }
-        }
-        for i in (0..n - 1).rev() {
-            let a = nums[i];
-            for (j, c) in count.iter_mut().enumerate() {
-                if a & (1 << j) > 0 {
-                    *c += 1;
-                }
-            }
-            result[i] = result[i + 1] + 1;
-            for k in (i + 1..i + 1 + result[i + 1] as usize).rev() {
-                if (0..32).all(|j| nums[k] & (1 << j) == 0 || count[j] > 1) {
-                    for (j, c) in count.iter_mut().enumerate() {
-                        if nums[k] & (1 << j) > 0 {
-                            *c -= 1;
-                        }
+        let mut ans = vec![0; n];
+        let (mut pre, mut suf, mut r, mut bottom) = (0, 0, n as i32 - 1, n as i32 - 1);
+        for l in (0..n).rev() {
+            suf |= nums[l];
+            pre |= nums[l];
+            while r >= l as i32 && pre | nums[r as usize] == suf {
+                r -= 1;
+                if bottom > r {
+                    for i in l as i32 + 1..=r {
+                        nums[i as usize] |= nums[i as usize - 1];
                     }
-                    result[i] -= 1;
-                } else {
-                    break;
+                    bottom = l as i32;
+                    pre = 0;
                 }
             }
+            ans[l] = r + 2 - l as i32;
         }
-        result
+        ans
     }
 }
 
@@ -46,5 +34,10 @@ mod tests {
             vec![3, 3, 2, 2, 1],
             Solution::smallest_subarrays(vec![1, 0, 2, 1, 3])
         );
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(vec![2, 1], Solution::smallest_subarrays(vec![1, 2]));
     }
 }
