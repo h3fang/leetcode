add42ef25238a995cbe3dbec7afa409fce5d607a
Fang He <hf.enigma@gmail.com>
2025-08-03 14:20:47 +0800

    p3638: minor optimization

--- rust/src/problems/p3638.rs
+++ rust/src/problems/p3638.rs
@@ -3,18 +3,14 @@ pub struct Solution;
 impl Solution {
     pub fn max_balanced_shipments(weight: Vec<i32>) -> i32 {
         let n = weight.len();
-        let (mut ans, mut i) = (0, 0);
-        while i + 1 < n {
-            let mut max = weight[i];
-            let mut j = i + 1;
-            while j < n && weight[j] >= max {
-                max = weight[j];
-                j += 1
-            }
-            if j < n {
+        let (mut ans, mut i) = (0, 1);
+        while i < n {
+            if weight[i - 1] < weight[i] {
                 ans += 1;
+                i += 2;
+            } else {
+                i += 1;
             }
-            i = j + 1;
         }
         ans
     }
