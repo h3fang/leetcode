4114b7def2e2971781b7541b8fcd6070b2bd55be
Fang He <hf.enigma@gmail.com>
2025-12-07 20:48:39 +0800

    p1523: optimization

--- rust/src/problems/p1523.rs
+++ rust/src/problems/p1523.rs
@@ -2,7 +2,7 @@ pub struct Solution;
 
 impl Solution {
     pub fn count_odds(low: i32, high: i32) -> i32 {
-        (high - low + 1) / 2 + if low % 2 == 1 && high % 2 == 1 { 1 } else { 0 }
+        (high + 1) / 2 - low / 2
     }
 }
 
