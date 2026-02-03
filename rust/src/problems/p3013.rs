cfee5e5533f4e18d69def60d78da63375c2bc49b
Fang He <hf.enigma@gmail.com>
2026-02-03 16:41:38 +0800

    p3013: refactor with BinaryHeap

--- rust/src/problems/p3013.rs
+++ rust/src/problems/p3013.rs
@@ -1,96 +1,105 @@
 pub struct Solution;
 
-use std::collections::BTreeMap;
+use std::collections::{BinaryHeap, HashMap};
 
-#[derive(Default)]
-struct Container {
-    k: usize,
-    st1: BTreeMap<i32, i32>,
-    st2: BTreeMap<i32, i32>,
-    pub sum: i64,
+struct LazyHeap {
+    heap: BinaryHeap<i32>,
+    to_remove: HashMap<i32, i32>,
     size: usize,
+    sum: i64,
 }
 
-impl Container {
-    fn remove_one(map: &mut BTreeMap<i32, i32>, key: i32) -> bool {
-        if let Some(e) = map.get_mut(&key) {
-            if *e > 1 {
-                *e -= 1;
-            } else {
-                map.remove(&key);
-            }
-            true
-        } else {
-            false
+impl LazyHeap {
+    fn with_capacity(cap: usize) -> Self {
+        Self {
+            heap: BinaryHeap::with_capacity(cap),
+            to_remove: HashMap::with_capacity(cap),
+            size: 0,
+            sum: 0,
         }
     }
 
-    fn add_one(map: &mut BTreeMap<i32, i32>, key: i32) {
-        *map.entry(key).or_insert(0) += 1;
+    fn push(&mut self, x: i32) {
+        self.heap.push(x);
+        self.size += 1;
+        self.sum += x as i64;
     }
 
-    fn adjust(&mut self) {
-        while self.size < self.k && !self.st2.is_empty() {
-            if let Some(&x) = self.st2.keys().next() {
-                Self::add_one(&mut self.st1, x);
-                Self::remove_one(&mut self.st2, x);
-                self.sum += x as i64;
-                self.size += 1;
-            }
-        }
-
-        while self.size > self.k {
-            if let Some(&x) = self.st1.keys().last() {
-                Self::add_one(&mut self.st2, x);
-                Self::remove_one(&mut self.st1, x);
-                self.sum -= x as i64;
-                self.size -= 1;
-            }
-        }
-    }
-
-    fn add(&mut self, x: i32) {
-        if !self.st2.is_empty() && x >= *self.st2.keys().next().unwrap() {
-            *self.st2.entry(x).or_insert(0) += 1;
-        } else {
-            *self.st1.entry(x).or_insert(0) += 1;
-            self.sum += x as i64;
-            self.size += 1;
-        }
-        self.adjust();
-    }
-
-    fn erase(&mut self, x: i32) {
-        if Self::remove_one(&mut self.st1, x) {
-            self.sum -= x as i64;
+    fn pop(&mut self) -> Option<i32> {
+        self.lazy_remove();
+        let v = self.heap.pop();
+        if let Some(x) = v {
             self.size -= 1;
-        } else {
-            Self::remove_one(&mut self.st2, x);
+            self.sum -= x as i64;
         }
-        self.adjust();
+        v
+    }
+
+    fn peek(&mut self) -> Option<&i32> {
+        self.lazy_remove();
+        self.heap.peek()
+    }
+
+    fn remove(&mut self, x: i32) {
+        self.size -= 1;
+        self.sum -= x as i64;
+        *self.to_remove.entry(x).or_default() += 1;
+    }
+
+    fn lazy_remove(&mut self) {
+        while let Some(&x) = self.heap.peek() {
+            if let Some(v) = self.to_remove.get_mut(&x)
+                && *v > 0
+            {
+                *v -= 1;
+                self.heap.pop();
+            } else {
+                break;
+            }
+        }
     }
 }
 
 impl Solution {
     pub fn minimum_cost(nums: Vec<i32>, k: i32, dist: i32) -> i64 {
-        let (k, dist) = (k as usize, dist as usize);
+        let (k, dist) = (k as usize - 1, dist as usize);
+        let (mut l, mut r) = (
+            LazyHeap::with_capacity(k),
+            LazyHeap::with_capacity(dist + 1),
+        );
 
-        let mut cnt = Container {
-            k: k - 2,
-            ..Default::default()
-        };
-        for &x in &nums[1..k - 1] {
-            cnt.add(x);
+        for &x in &nums[1..dist + 2] {
+            l.push(x);
         }
 
-        let mut ans = cnt.sum + nums[k - 1] as i64;
-        for (i, &x) in nums.iter().enumerate().skip(k) {
-            let j = i as i32 - dist as i32 - 1;
-            if j > 0 {
-                cnt.erase(nums[j as usize]);
+        while l.size > k {
+            let x = l.pop().unwrap();
+            r.push(-x);
+        }
+
+        let mut ans = l.sum;
+
+        for (i, &x) in nums.iter().enumerate().skip(dist + 2) {
+            if x < *l.peek().unwrap() {
+                l.push(x);
+                if l.size > k {
+                    r.push(-l.pop().unwrap());
+                }
+            } else {
+                r.push(-x);
             }
-            cnt.add(nums[i - 1]);
-            ans = ans.min(cnt.sum + x as i64);
+
+            let left = nums[i - dist - 1];
+            if left <= *l.peek().unwrap() {
+                l.remove(left);
+                if l.size < k {
+                    l.push(-r.pop().unwrap());
+                }
+            } else {
+                r.remove(-left);
+            }
+
+            ans = ans.min(l.sum);
         }
 
         ans + nums[0] as i64
@@ -114,5 +123,29 @@ mod tests {
     #[test]
     fn case3() {
         assert_eq!(36, Solution::minimum_cost(vec![10, 8, 18, 9], 3, 1));
+    }
+
+    #[test]
+    fn case4() {
+        assert_eq!(9, Solution::minimum_cost(vec![1, 5, 3, 6], 3, 2));
+    }
+
+    #[test]
+    fn case5() {
+        assert_eq!(9, Solution::minimum_cost(vec![1, 5, 3, 7], 3, 1));
+    }
+
+    #[test]
+    fn case6() {
+        assert_eq!(
+            163,
+            Solution::minimum_cost(
+                vec![
+                    6, 40, 41, 11, 50, 15, 47, 48, 50, 12, 16, 30, 38, 45, 13, 34, 26, 25, 32, 28
+                ],
+                9,
+                13
+            )
+        );
     }
 }
