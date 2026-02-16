e835c9db9e6eccf82b98a8bb7c1e3673d560b38f
Fang He <hf.enigma@gmail.com>
2026-02-16 13:24:08 +0800

    p190: use builtin function

--- rust/src/problems/p190.rs
+++ rust/src/problems/p190.rs
@@ -1,14 +1,8 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn reverse_bits(mut x: u32) -> u32 {
-        let mut r = 0;
-        for _ in 0..32 {
-            r <<= 1;
-            r += x & 1;
-            x >>= 1;
-        }
-        r
+    pub fn reverse_bits(x: i32) -> i32 {
+        x.reverse_bits()
     }
 }
 
@@ -18,17 +12,11 @@ mod tests {
 
     #[test]
     fn case1() {
-        assert_eq!(
-            964176192,
-            Solution::reverse_bits(0b00000010100101000001111010011100)
-        );
+        assert_eq!(964176192, Solution::reverse_bits(43261596));
     }
 
     #[test]
     fn case2() {
-        assert_eq!(
-            1,
-            Solution::reverse_bits(0b10000000000000000000000000000000)
-        );
+        assert_eq!(1073741822, Solution::reverse_bits(2147483644));
     }
 }
