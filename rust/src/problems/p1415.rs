3825d5798a0458b93d1d1969c10826c4c2821f38
Fang He <hf.enigma@gmail.com>
2026-03-14 21:53:19 +0800

    p1415: minor optimization

--- rust/src/problems/p1415.rs
+++ rust/src/problems/p1415.rs
@@ -2,37 +2,19 @@ pub struct Solution;
 
 impl Solution {
     pub fn get_happy_string(n: i32, mut k: i32) -> String {
-        let mut m = 1 << (n - 1);
-        if k > 3 * m {
+        if k > 3 << (n - 1) {
             return String::new();
         }
         k -= 1;
         let mut result = Vec::with_capacity(n as usize);
-        if k < m {
-            result.push(b'a');
-        } else if k < 2 * m {
-            result.push(b'b');
-        } else {
-            result.push(b'c');
-        };
-        k %= m;
-        m /= 2;
-        for _ in 2..=n {
-            let j = if k < m { 1 } else { 2 };
-            let last = *result.last().unwrap();
-            if last == b'b' {
-                if j == 1 {
-                    result.push(b'a');
-                } else {
-                    result.push(b'c');
-                }
-            } else {
-                result.push(b'a' + (last - b'a' + j) % 3);
-            }
-            k %= m;
-            m /= 2;
+        let mut last = b'a' + (k >> (n - 1)) as u8;
+        result.push(last);
+        for i in 1..n {
+            let b = b'a' + ((k >> (n - 1 - i)) & 1) as u8;
+            last = b + u8::from(b >= last);
+            result.push(last);
         }
-        String::from_utf8(result).unwrap()
+        unsafe { String::from_utf8_unchecked(result) }
     }
 }
 
