f1839362773f41dc07458280d423d7b3d017c220
Fang He <hf.enigma@gmail.com>
2026-03-26 23:31:43 +0800

    p3548: minor optimization

--- rust/src/problems/p3548.rs
+++ rust/src/problems/p3548.rs
@@ -4,32 +4,35 @@ use std::collections::HashSet;
 
 fn check_rows(grid: &[Vec<i32>]) -> bool {
     let (m, n) = (grid.len(), grid[0].len());
-    let mut presum = Vec::with_capacity(m);
 
-    for r in grid {
-        let sum: i64 = r.iter().map(|&e| e as i64).sum();
-        presum.push(*presum.last().unwrap_or(&0) + sum);
-    }
-
-    let total = *presum.last().unwrap();
+    let total = grid.iter().flatten().map(|&v| v as i64).sum::<i64>();
     let mut nums = HashSet::with_capacity(m * n);
+    let (mut sum, mut max) = (0, 0);
 
-    for (i, &s) in presum.iter().enumerate().take(m - 1) {
-        if 2 * s == total {
+    for (i, r) in grid.iter().enumerate().take(m - 1) {
+        for &v in r {
+            sum += v as i64;
+            max = max.max(v);
+        }
+        if 2 * sum == total {
             return true;
+        }
+        if 2 * sum > total + max as i64 {
+            break;
         }
         for &x in &grid[i] {
             nums.insert(x);
         }
-        let Ok(x) = i32::try_from(2 * s - total) else {
+        let Ok(x) = i32::try_from(2 * sum - total) else {
             continue;
         };
-        if i == 0 {
-            if n > 1 && x == grid[0][0] || x == grid[0][n - 1] {
+
+        if n == 1 {
+            if x == grid[0][0] || x == grid[i][0] {
                 return true;
             }
-        } else if n == 1 {
-            if x == grid[0][0] || x == grid[i][0] {
+        } else if i == 0 {
+            if x == grid[0][0] || x == grid[0][n - 1] {
                 return true;
             }
         } else if nums.contains(&x) {
@@ -39,54 +42,34 @@ fn check_rows(grid: &[Vec<i32>]) -> bool {
     false
 }
 
-fn check_cols(grid: &[Vec<i32>]) -> bool {
+fn rotate_90cw(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let (m, n) = (grid.len(), grid[0].len());
-    let mut presum = Vec::with_capacity(n);
-
-    for j in 0..grid[0].len() {
-        let sum: i64 = grid.iter().map(|r| r[j] as i64).sum();
-        presum.push(*presum.last().unwrap_or(&0) + sum);
-    }
-
-    let total = *presum.last().unwrap();
-    let mut nums = HashSet::with_capacity(m * n);
-
-    for (j, &s) in presum.iter().enumerate().take(n - 1) {
-        if 2 * s == total {
-            return true;
-        }
-        for r in grid {
-            nums.insert(r[j]);
-        }
-        let Ok(x) = i32::try_from(2 * s - total) else {
-            continue;
-        };
-        if j == 0 {
-            if m > 1 && x == grid[0][j] || x == grid[m - 1][j] {
-                return true;
-            }
-        } else if m == 1 {
-            if x == grid[0][0] || x == grid[0][j] {
-                return true;
-            }
-        } else if nums.contains(&x) {
-            return true;
+    let mut ans = vec![vec![0; m]; n];
+    for (i, r) in grid.into_iter().enumerate() {
+        for (j, c) in r.into_iter().enumerate() {
+            ans[j][m - 1 - i] = c;
         }
     }
-    false
+    ans
 }
 
 impl Solution {
     pub fn can_partition_grid(mut grid: Vec<Vec<i32>>) -> bool {
-        if check_cols(&grid) || check_rows(&grid) {
+        fn check(grid: &mut [Vec<i32>]) -> bool {
+            if check_rows(grid) {
+                return true;
+            }
+            grid.reverse();
+            check_rows(grid)
+        }
+
+        if check(&mut grid) {
             return true;
         }
-        grid.reverse();
-        if check_rows(&grid) {
-            return true;
-        }
-        grid.iter_mut().for_each(|r| r.reverse());
-        check_cols(&grid)
+
+        let mut grid = rotate_90cw(grid);
+
+        check(&mut grid)
     }
 }
 
