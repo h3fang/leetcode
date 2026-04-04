89fe3016cf8ff46fce422355718df5578a86248e
Fang He <hf.enigma@gmail.com>
2026-04-04 17:34:07 +0800

    p3661: optimization

--- rust/src/problems/p3661.rs
+++ rust/src/problems/p3661.rs
@@ -2,7 +2,7 @@ pub struct Solution;
 
 impl Solution {
     pub fn max_walls(robots: Vec<i32>, distance: Vec<i32>, mut walls: Vec<i32>) -> i32 {
-        let n = robots.len();
+        let (m, n) = (walls.len(), robots.len());
         let mut rds = robots.into_iter().zip(distance).collect::<Vec<_>>();
         rds.push((0, 0));
         rds.push((i32::MAX, 0));
@@ -11,21 +11,30 @@ impl Solution {
 
         let mut f = [0; 2];
 
+        let (mut left, mut curr, mut right) = (0, 0, [0; 2]);
         for (i, &(x, d)) in rds.iter().enumerate().skip(1).take(n) {
             let left_x = (x - d).max(rds[i - 1].0 + 1);
-            let left = walls.partition_point(|&w| w < left_x) as i32;
-            let curr = walls.partition_point(|&w| w <= x) as i32;
-            let left = f[0] + curr - left;
-
-            let curr = walls.partition_point(|&w| w < x) as i32;
+            while left < m && walls[left] < left_x {
+                left += 1;
+            }
+            while curr < m && walls[curr] < x {
+                curr += 1;
+            }
+            let curr1 = curr as i32;
+            while curr < m && walls[curr] == x {
+                curr += 1;
+            }
+            let left_walls = f[0] + curr as i32 - left as i32;
             for j in 0..2 {
                 let (mut x2, d2) = rds[i + 1];
                 if j == 0 {
                     x2 -= d2;
                 }
                 let right_x = (x + d).min(x2 - 1);
-                let right = walls.partition_point(|&w| w <= right_x) as i32;
-                f[j] = left.max(f[1] + right - curr);
+                while right[j] < m && walls[right[j]] <= right_x {
+                    right[j] += 1;
+                }
+                f[j] = left_walls.max(f[1] + right[j] as i32 - curr1);
             }
         }
 
