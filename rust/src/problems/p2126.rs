7f7dd208bb84bb34c97937f01c66dd71c33c9681
Fang He <hf.enigma@gmail.com>
2026-05-31 23:09:43 +0800

    p2126: optimization

--- rust/src/problems/p2126.rs
+++ rust/src/problems/p2126.rs
@@ -1,15 +1,31 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn asteroids_destroyed(mass: i32, mut asteroids: Vec<i32>) -> bool {
+    pub fn asteroids_destroyed(mass: i32, asteroids: Vec<i32>) -> bool {
+        let max = *asteroids.iter().max().unwrap();
+        let width = 32 - max.leading_zeros() as usize;
+        let mut mins = vec![i32::MAX; width];
+        let mut sums = vec![0; width];
+
+        for x in asteroids {
+            let w = 32 - x.leading_zeros() as usize - 1;
+            mins[w] = mins[w].min(x);
+            sums[w] += x as i64;
+        }
+
         let mut m = mass as i64;
-        asteroids.sort_unstable();
-        for a in asteroids {
-            if m < a as i64 {
+        for (min, sum) in mins.into_iter().zip(sums) {
+            if min == i32::MAX {
+                continue;
+            }
+
+            if m < min as i64 {
                 return false;
             }
-            m += a as i64;
+
+            m += sum;
         }
+
         true
     }
 }
