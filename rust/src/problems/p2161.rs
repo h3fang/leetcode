03bfab95b0c24a20d79e528baf41b8772b73c08e
Fang He <hf.enigma@gmail.com>
2026-06-08 23:33:54 +0800

    p2161: minor refactor

--- rust/src/problems/p2161.rs
+++ rust/src/problems/p2161.rs
@@ -1,18 +1,25 @@
 pub struct Solution;
 
+use std::cmp::Ordering;
+
 impl Solution {
     pub fn pivot_array(nums: Vec<i32>, pivot: i32) -> Vec<i32> {
-        let mut result = Vec::with_capacity(nums.len());
-        let mut right = Vec::new();
+        let mut result = vec![pivot; nums.len()];
+        let (mut l, mut r) = (0, nums.len() - 1);
         for &n in &nums {
             match n.cmp(&pivot) {
-                std::cmp::Ordering::Less => result.push(n),
-                std::cmp::Ordering::Equal => {}
-                std::cmp::Ordering::Greater => right.push(n),
+                Ordering::Less => {
+                    result[l] = n;
+                    l += 1;
+                }
+                Ordering::Equal => {}
+                Ordering::Greater => {
+                    result[r] = n;
+                    r = r.saturating_sub(1);
+                }
             }
         }
-        result.extend(vec![pivot; nums.len() - result.len() - right.len()]);
-        result.extend(right);
+        result[r + 1..].reverse();
         result
     }
 }
