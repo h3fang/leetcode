86cc48ce607ed9983979b2b680a5c842af3fd9aa
Fang He <hf.enigma@gmail.com>
2026-07-19 09:43:52 +0800

    p316: minor optimization

--- rust/src/problems/p316.rs
+++ rust/src/problems/p316.rs
@@ -7,24 +7,24 @@ impl Solution {
             let idx = (c as u8 - b'a') as usize;
             last_index[idx] = i;
         }
-        let mut visited = [false; 26];
+        let mut visited = 0u32;
         let mut stack = vec![];
         for (i, c) in s.char_indices() {
-            let idx = (c as u8 - b'a') as usize;
-            if visited[idx] {
+            let idx = c as u8 - b'a';
+            if visited & (1 << idx) > 0 {
                 continue;
             }
             while let Some(&top) = stack.last() {
                 let idx_top = (top as u8 - b'a') as usize;
                 if top > c && last_index[idx_top] > i {
                     stack.pop();
-                    visited[idx_top] = false;
+                    visited &= !(1 << idx_top);
                 } else {
                     break;
                 }
             }
             stack.push(c);
-            visited[idx] = true;
+            visited |= 1 << idx;
         }
         stack.iter().collect()
     }
