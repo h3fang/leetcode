894ea0808bf5cc28d12df7f55f8b7538a79e974e
Fang He <hf.enigma@gmail.com>
2026-07-24 10:06:33 +0800

    p3514: optimization

--- rust/src/problems/p3514.rs
+++ rust/src/problems/p3514.rs
@@ -1,31 +1,38 @@
 pub struct Solution;
 
+fn fwt_xor(a: &mut [i64], shift: u32) {
+    let n = a.len();
+
+    let (mut l, mut k) = (2, 1);
+    while l <= n {
+        for i in (0..n).step_by(l) {
+            for j in 0..k {
+                (a[i + j], a[i + j + k]) = (
+                    (a[i + j] + a[i + j + k]) >> shift,
+                    (a[i + j] - a[i + j + k]) >> shift,
+                );
+            }
+        }
+
+        (l, k) = (l << 1, k << 1);
+    }
+}
+
 impl Solution {
-    pub fn unique_xor_triplets(mut nums: Vec<i32>) -> i32 {
-        nums.sort_unstable();
-        nums.dedup();
-        let max = *nums.last().unwrap();
+    pub fn unique_xor_triplets(nums: Vec<i32>) -> i32 {
+        let max = *nums.iter().max().unwrap();
         let max = 1usize << (i32::BITS - max.leading_zeros());
 
-        let mut pairs = vec![false; max];
-        pairs[0] = true;
-        for (i, &x) in nums.iter().enumerate() {
-            for &y in &nums[i + 1..] {
-                pairs[(x ^ y) as usize] = true;
-            }
-        }
-
-        let mut triplets = vec![false; max];
-
+        let mut cnt = vec![0; max];
         for x in nums {
-            for (y, &b) in pairs.iter().enumerate() {
-                if b {
-                    triplets[(x ^ y as i32) as usize] = true;
-                }
-            }
+            cnt[x as usize] += 1;
         }
 
-        triplets.into_iter().filter(|&b| b).count() as i32
+        fwt_xor(&mut cnt, 0);
+        cnt.iter_mut().for_each(|e| *e = *e * *e * *e);
+        fwt_xor(&mut cnt, 1);
+
+        cnt.into_iter().filter(|&e| e > 0).count() as i32
     }
 }
 
