6437b418a8415f9baed71e34dd6ee1c39e335dea
Fang He <hf.enigma@gmail.com>
2026-09-23 23:10:47 +0800

    p2744

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -1201,6 +1201,7 @@ pub mod p274;
 pub mod p2740;
 pub mod p2741;
 pub mod p2742;
+pub mod p2744;
 pub mod p2749;
 pub mod p275;
 pub mod p2751;
---
+++ rust/src/problems/p2744.rs
@@ -0,0 +1,45 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn maximum_number_of_string_pairs(words: Vec<String>) -> i32 {
+        let mut set = vec![false; 26 * 26];
+        let mut result = 0;
+        for w in words {
+            let a = w.as_bytes();
+            let hash = u16::from(a[0] - b'a') * 26 + u16::from(a[1] - b'a');
+            if set[hash as usize] {
+                result += 1;
+            }
+
+            let hash = u16::from(a[1] - b'a') * 26 + u16::from(a[0] - b'a');
+            set[hash as usize] = true;
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let words = ["cd", "ac", "dc", "ca", "zz"]
+            .iter()
+            .map(|w| w.to_string())
+            .collect();
+        assert_eq!(2, Solution::maximum_number_of_string_pairs(words));
+    }
+
+    #[test]
+    fn case2() {
+        let words = ["ab", "ba", "cc"].iter().map(|w| w.to_string()).collect();
+        assert_eq!(1, Solution::maximum_number_of_string_pairs(words));
+    }
+
+    #[test]
+    fn case3() {
+        let words = ["aa", "ab"].iter().map(|w| w.to_string()).collect();
+        assert_eq!(0, Solution::maximum_number_of_string_pairs(words));
+    }
+}
